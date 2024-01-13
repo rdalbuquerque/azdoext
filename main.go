d3ebd3d1dfe4bfc077aa40c518d2875a019fa8eb
name <email>
2024-01-13 14:28:08 -0300

    add items to pipelines

--- main.go
+++ main.go
@@ -89,6 +89,7 @@ func (m *model) fetchPipelines() tea.Msg {
 		pipeline := pipeline.(map[string]interface{})["name"].(string)
 		items = append(items, item(pipeline))
 	}
+	m.pipelines = list.New(items, list.NewDefaultDelegate(), 0, 0)
 	return gitOutputMsg("Pipelines fetched")
 }
 
